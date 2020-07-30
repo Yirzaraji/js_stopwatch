a8e791a6c2413fba917d79358c059c7de19363b7
Remi <not.iremy@gmail.com>
2020-07-30 23:12:44 +0200

    Creation visuel css + ajout javascript objet Chrono

--- js/app.js
+++ js/app.js
@@ -1,47 +1,71 @@
-let startChrono = 1900
-let endChrono = 2000
+//let startChrono = 1900
+//let endChrono = 2000
 
+/* for (const chrono of chronos) {
+chrono.innerHTML = "0000" + " Y "
+} */
+
+/*
 const chronos = document.getElementsByClassName('chrono')
 const startBtn = document.getElementsByClassName('startChronoBtn')
 const stopBtn = document.getElementsByClassName('stopChronoBtn')
 const resetBtn = document.getElementsByClassName('resetChronoBtn')
+ */
 
-for (const start of startBtn) {
-    start.addEventListener('click', () =>
-        addYear = setInterval(() => {
-            for (const chrono of chronos) {
-                console.log(chrono);
-                if (startChrono < 2001) {
-                    chrono.innerHTML = startChrono++
-                        chrono.style.fontSize = "3em";
-                } else {
-                    clearInterval(addYear);
-                }
-            };
-        }, 100)
+/* function start(){
+    for (const start of startBtn) {
+        start.addEventListener('click', () =>
+            addYear = setInterval(() => {
+                for (const chrono of chronos) {
+                    console.log(chrono);
+                    if (startChrono < 2001) {
+                        chrono.innerHTML = startChrono++
+                        
+                    } else {
+                        clearInterval(addYear);
+                    }
+                };
+            }, 100)
 
-    );
+        );
 
-};
-
-for (const stop of stopBtn) {
-    stop.addEventListener('click', () =>
-        clearInterval(addYear)
-    )
+    };
+}
+function pause(){
+    for (const stop of stopBtn) {
+        stop.addEventListener('click', () =>
+            clearInterval(addYear)
+        )
+    }
+}
+function reset(){
+    for (const chrono of chronos) {
+        for (const reset of resetBtn) {
+            reset.addEventListener('click', (event) =>
+            window.location.reload()
+            )
+        }
+    }
 }
 
-for (const reset of resetBtn) {
-    reset.addEventListener('click', () =>
-        clearInterval(addYear)
+start()
+pause()
+reset()
+ */
 
-        /*   for (const chrono of chronos) {
-              chronos.innerHTML = startChrono
-          } */
-    );
+
+//Partie chrono objet
+
+
+const timer = new Chrono(start = 1900, msInterval = 100);
+timer.startTimer();
+//timer.pauseTimer()
+timer.resetTimer()
+//console.log(timer.start)
+/* for(const test of tests){
+    
+    console.log(timer.start)
+    test.innerHTML = timer.start
+   
 }
-
-
-const timer = new Chrono(start = 1900, end = 2000, interval = 100);
-console.log(timer.startTimer)
-
-//setInterval(function() { alert("Hello"); }, 3000);
+ */
--- js/chrono.js
+++ js/chrono.js
@@ -1,20 +1,56 @@
 class Chrono {
 
-    constructor(startYear, endYear, intervalMs) {
+    constructor(startYear, msInterval) {
         this.start = startYear
-        this.end = endYear
-        this.interval = intervalMs
+        this.interval = msInterval
     }
 
 
     startTimer() {
-        this.start++
+        //Initialise le compteur a 0000
+        const chronos = document.getElementsByClassName('chrono')
+        for (const chrono of chronos) {
+        chrono.innerHTML = "0000" + " Y "
+        }
 
+        //interval de 100ms Incrementation +1
+        const startBtn = document.getElementById('startChronoBtn')
+        startBtn.addEventListener('click', () =>
+            setInterval(() => {   
+                if(this.start < 2000) {
+                    this.start++
+                    console.log(this.start)
+                    const test = document.getElementById('test')
+                    test.innerHTML = this.start + " Y"
+                }
+                else{
+                    clearInterval();
+                }
+            }, msInterval)
+        );
     }
 
-    endTimer() {
-        console.log('coucou2')
+    pauseTimer(){
+        console.log('pause')
+        let stopBtn = document.getElementById('stopChronoBtn')
+            stopBtn.addEventListener('click', () =>
+            console.log("test click"),
+                clearInterval()
+            )
+        
     }
+
+   resetTimer() {
+        const resetBtn = document.getElementsByClassName('resetChronoBtn')
+            for (const reset of resetBtn) {
+                reset.addEventListener('click', (event) =>
+                console.log('reset'),
+                //window.location.reload()
+                )
+            }
+        }
+
+
 
 }
 
